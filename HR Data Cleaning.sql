bca471d42e0670aeeeb50f4a99c49ba4d74a28c2
Haddouche Maamar <frocodeof@gmail.com>
2023-12-21 22:18:24 +0100

    START UPDATING DATA

---
+++ HR Data Cleaning.sql
@@ -0,0 +1,18 @@
+USE project;
+
+
+SELECT birthdate FROM hr;
+
+SET sql_safe_updates = 0;
+
+ALTER TABLE hr
+CHANGE COLUMN id emp_id VARCHAR(20) NULL;
+
+UPDATE hr
+SET birthdate = CASE
+	WHEN birthdate LIKE '%/%' THEN date_format(str_to_date(birthdate, '%m/%d/%Y'), '%Y-%m-%d')
+    WHEN birthdate LIKE '%-%' THEN date_format(str_to_date(birthdate, '%m-%d-%Y'), '%Y-%m-%d')
+    ELSE NULL
+END;
+
+
