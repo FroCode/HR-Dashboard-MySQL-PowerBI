ec2ea6dcf61bdf9bccdab5817a58249e55039aac
Haddouche Maamar <frocodeof@gmail.com>
2023-12-21 22:32:33 +0100

    MODIFY HIRE_DATE COLUMN

--- HR Data Cleaning.sql
+++ HR Data Cleaning.sql
@@ -1,12 +1,15 @@
 USE project;
 
+SELECT * FROM hr;
+
+ALTER TABLE hr
+CHANGE COLUMN id emp_id VARCHAR(20) NULL;
+
+DESCRIBE hr;
 
 SELECT birthdate FROM hr;
 
 SET sql_safe_updates = 0;
-
-ALTER TABLE hr
-CHANGE COLUMN id emp_id VARCHAR(20) NULL;
 
 UPDATE hr
 SET birthdate = CASE
@@ -15,4 +18,16 @@ SET birthdate = CASE
     ELSE NULL
 END;
 
+ALTER TABLE hr
+MODIFY COLUMN birthdate DATE;
 
+UPDATE hr
+SET hire_date = CASE
+	WHEN hire_date LIKE '%/%' THEN date_format(str_to_date(hire_date, '%m/%d/%Y'), '%Y-%m-%d')
+    WHEN hire_date LIKE '%-%' THEN date_format(str_to_date(hire_date, '%m-%d-%Y'), '%Y-%m-%d')
+    ELSE NULL
+END;
+
+ALTER TABLE hr
+MODIFY COLUMN hire_date DATE;
+SELECT hr.termdate FROM hr;
