1977be98476351a9af3ec04be1f958c0f4943e6e
Haddouche Maamar <frocodeof@gmail.com>
2023-12-22 12:53:25 +0100

    - Question 3 : Done -Export and Add CSV Race Result3

--- HR DATA QUESTIONS.sql
+++ HR DATA QUESTIONS.sql
@@ -6,7 +6,7 @@ USE project  ;
     SELECT gender , count(*) AS count
     FROM  hr
     WHERE age >= 18 AND termdate = '0000-00-00'
-    GROUP BY gender
+    GROUP BY gender;
 
 -- 2. What is the race/ethnicity breakdown of employees in the company?
 
@@ -17,6 +17,28 @@ USE project  ;
     ORDER BY count(*) DESC;
 
 -- 3. What is the age distribution of employees in the company?
+
+    SELECT
+        MIN(age) AS youngest ,
+        MAX(age) AS oldest
+    FROM hr
+    WHERE age>= 18 AND termdate = '0000-00-00';
+
+    SELECT
+        CASE
+            WHEN age >= 18 AND age <= 24 THEN '18-24'
+            WHEN age >= 25 AND age <= 34 THEN '25-34'
+            WHEN age >= 35 AND age <= 45 THEN '35-45'
+            WHEN age >= 45 AND age <= 54 THEN '45-54'
+            WHEN age >= 55 AND age <= 64 THEN '55-64'
+    ELSE '+65'
+    END AS age_group,
+        count(*) AS count
+    FROM hr
+        WHERE age>= 18 AND termdate = '0000-00-00'
+GROUP BY age_group
+    ORDER BY age_group;
+
 
 
 -- 4. How many employees work at headquarters versus remote locations?
