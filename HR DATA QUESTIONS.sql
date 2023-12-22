df703cdac5bdeeb0c59317461c24bee474ccc8e6
Haddouche Maamar <frocodeof@gmail.com>
2023-12-22 12:59:23 +0100

    - Question 3-2(ADD GENDER) : Done -Export and Add CSV Race Result3

--- HR DATA QUESTIONS.sql
+++ HR DATA QUESTIONS.sql
@@ -40,6 +40,25 @@ GROUP BY age_group
     ORDER BY age_group;
 
 
+#ADD GENDER RESULT
+
+
+     SELECT
+        CASE
+            WHEN age >= 18 AND age <= 24 THEN '18-24'
+            WHEN age >= 25 AND age <= 34 THEN '25-34'
+            WHEN age >= 35 AND age <= 45 THEN '35-45'
+            WHEN age >= 45 AND age <= 54 THEN '45-54'
+            WHEN age >= 55 AND age <= 64 THEN '55-64'
+    ELSE '+65'
+    END AS age_group,gender,
+        count(*) AS count
+    FROM hr
+        WHERE age>= 18 AND termdate = '0000-00-00'
+GROUP BY gender
+    ORDER BY gender;
+
+
 
 -- 4. How many employees work at headquarters versus remote locations?
 
